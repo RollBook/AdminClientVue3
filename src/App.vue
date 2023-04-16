4073ed800e5d26f2c3764e271d52b29644b39848
FAll <940313262@qq.com>
2023-04-16 21:44:36 +0800

    fix: 修复router导入错误

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <router-view />
+  <router-view :key="useRoute().fullPath" />
 </template>
 
 <script setup lang="ts">
 // TODO: 导入语言包
+import {useRoute} from 'vue-router';
 
 </script>
 
 <style lang="less" scoped>
+
 
 </style>
